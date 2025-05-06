9c266c003895b106dd59cd205d29f143ec46733b
GeorgeKreye <102697450+GeorgeKreye@users.noreply.github.com>
2025-05-06 16:14:47 -0400

    Save progress so far

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,27 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(JSON.stringify(books[isbn],null,4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let authorBooks = {};
+  keys = Object.keys(books);
+  for (key in keys) {
+    const book = books[key];
+    if (book && book['author'] == author) {
+        authorBooks[key] = book;
+    }
+  }
+  res.send(JSON.stringify(authorBooks,null,4));
 });
 
 // Get all books based on title
